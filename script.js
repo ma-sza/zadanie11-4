ecfd5f9d4315ad47f43e5ce7f95768d51b2e1f1e
Maria Ulanicka <maria.ulanicka@gmail.com>
2018-03-18 13:26:20 +0100

    Add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function Phone(brand, price, color) {
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+  console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is $" + this.price + ".");
+};
+
+var iPhone6s = new Phone("Apple", 2250, "silver");
+var SamsungGalaxyS6 = new Phone("Samsung", 2100, "black");
+var OnePlusOne = new Phone("One", 1500, "white");
+
+iPhone6s.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
